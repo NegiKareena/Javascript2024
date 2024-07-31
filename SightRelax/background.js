e9f5937531d9956478a3c51bda90dcc1ae311779
Kareena Negi <159912897+NegiKareena@users.noreply.github.com>
2024-07-31 20:41:08 +0530

    Add files via upload

---
+++ SightRelax/background.js
@@ -0,0 +1,16 @@
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    chrome.notifications.create("confirm", {
+      message: "Select an option", //can be replaced
+      type: "basic",
+      title: "Ready to relax your eyes?",
+      iconUrl: "./icons/search.png",
+      buttons: [{ title: "Yes" }, { title: "No" }],
+      requireInteraction: true,
+    });
+  
+    chrome.notifications.onButtonClicked.addListener((_, buttonIndex) => {
+      if (buttonIndex === 0) chrome.tabs.create({ url: "background.html" });
+      chrome.notifications.clear("confirm");
+    });
+  });
+  
